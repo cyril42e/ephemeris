cca67dc03e1bf66b98d7daf67e6b1f39f7e590bd
Cyril Roussillon <cyril42e@gmail.com>
2024-10-10 21:34:14 +0000

    Cache CDN resources as well (required for offline usage)
    will need to avoid duplication of URL with the html file...

--- serviceWorker.js
+++ serviceWorker.js
@@ -8,7 +8,13 @@ self.addEventListener('install', event => {
                 './styles.css',
                 './script.js',
                 './icon-192.png',
-                './icon-512.png'
+                './icon-512.png',
+                'https://cdn.jsdelivr.net/npm/luxon@3.5.0/build/global/luxon.min.js',
+                'https://cdn.jsdelivr.net/npm/tz-lookup@6.1.25/tz.min.js',
+                'https://cdn.jsdelivr.net/npm/astronomy-engine@2.1.19/astronomy.browser.min.js',
+                'https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.js',
+                'https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.css',
+                'https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/themes/dark.min.css'
             ]);
         })
     );
